0b2d476dba36cb4862f75fffadceeea30eec58ac
koteyka <tihonenkovk@mail.ru>
2023-07-01 23:21:15 +0700

    Update project

--- src/main/java/pageObjects/settings/SettingsPage.java
+++ src/main/java/pageObjects/settings/SettingsPage.java
@@ -1,8 +1,8 @@
 package pageObjects.settings;
 
-import io.appium.java_client.MobileElement;
+import org.openqa.selenium.WebElement;
 
-import static util.Driver.mobileElementByXpath;
+import static util.Driver.WebElementByXpath;
 
 /**
  * Страница "Настройки"
@@ -12,7 +12,7 @@ public class SettingsPage {
      * Поле "Язык"
      * @param languageName - "название" языка
      */
-    public MobileElement language(String languageName) {
-        return mobileElementByXpath("//android.widget.TextView[@text='" + languageName + "']");
+    public WebElement language(String languageName) {
+        return WebElementByXpath("//android.widget.TextView[@text='" + languageName + "']");
     }
 }
--- src/main/java/util/Driver.java
+++ src/main/java/util/Driver.java
@@ -1,7 +1,8 @@
 package util;
 
-import io.appium.java_client.MobileElement;
+import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -11,142 +12,134 @@ import java.util.List;
 import static io.qameta.allure.Allure.step;
 
 public class Driver {
-    public static AndroidDriver<MobileElement> driver;
-    private static Device device;
+	public static AndroidDriver driver;
+	private static Device device;
 
-    /**
-     * Поиск элемента по Id
-     * Драйвер будет установлен автоматически, если это не было сделано ранее
-     */
-    public static WebElement byId(String locator) {
-        if (driver == null) setUpDriver();
-        return driver.findElementById(locator);
-    }
+	/**
+	 * Поиск элемента по Id
+	 * Драйвер будет установлен автоматически, если это не было сделано ранее
+	 */
+	public static WebElement byId(String locator) {
+		if (driver == null) setUpDriver();
+		return driver.findElement(By.id(locator));
+	}
 
-    /**
-     * Поиск элемента по Id
-     * Драйвер будет установлен автоматически, если это не было сделано ранее
-     */
-    public static MobileElement mobileElementById(String locator) {
-        return (MobileElement) byId(locator);
-    }
+	/**
+	 * Поиск элемента по Id
+	 * Драйвер будет установлен автоматически, если это не было сделано ранее
+	 */
+	public static WebElement WebElementById(String locator) {
+		return (WebElement) byId(locator);
+	}
 
-    /**
-     * Поиск элемента по CSS
-     * Драйвер будет установлен автоматически, если это не было сделано ранее
-     */
-    public static List<MobileElement> mobileElementsByClassName(String locator){
-        if (driver == null) setUpDriver();
-        return driver.findElementsByClassName(locator);
-    }
+	/**
+	 * Поиск элемента по CSS
+	 * Драйвер будет установлен автоматически, если это не было сделано ранее
+	 */
+	public static List<WebElement> WebElementsByClassName(String locator) {
+		if (driver == null) setUpDriver();
+		return driver.findElements(By.className(locator));
+	}
 
-    /**
-     * Поиск списка элементов по Id
-     * Драйвер будет установлен автоматически, если это не было сделано ранее
-     */
-    public static List<MobileElement> mobileElementsById(String locator){
-        if (driver == null) setUpDriver();
-        return driver.findElementsById(locator);
-    }
+	/**
+	 * Поиск списка элементов по Id
+	 * Драйвер будет установлен автоматически, если это не было сделано ранее
+	 */
+	public static List<WebElement> WebElementsById(String locator) {
+		if (driver == null) setUpDriver();
+		return driver.findElements(By.id(locator));
+	}
 
-    /**
-     * Поиск элемента по AccessibilityId
-     * Драйвер будет установлен автоматически, если это не было сделано ранее
-     */
-    public static MobileElement mobileElementByAccessibilityId(String locator) {
-        if (driver == null) setUpDriver();
-        return driver.findElementByAccessibilityId(locator);
-    }
+	/**
+	 * Поиск элемента по xpath
+	 * Драйвер будет установлен автоматически, если это не было сделано ранее
+	 */
+	public static WebElement byXpath(String locator) {
+		if (driver == null) setUpDriver();
+		return driver.findElement(By.xpath(locator));
+	}
 
-    /**
-     * Поиск элемента по xpath
-     * Драйвер будет установлен автоматически, если это не было сделано ранее
-     */
-    public static WebElement byXpath(String locator) {
-        if (driver == null) setUpDriver();
-        return driver.findElementByXPath(locator);
-    }
+	/**
+	 * Поиск мобильного элемента по xpath
+	 * Драйвер будет установлен автоматически, если это не было сделано ранее
+	 */
+	public static WebElement WebElementByXpath(String locator) {
+		if (driver == null) setUpDriver();
+		return byXpath(locator);
+	}
 
-    /**
-     * Поиск мобильного элемента по xpath
-     * Драйвер будет установлен автоматически, если это не было сделано ранее
-     */
-    public static MobileElement mobileElementByXpath(String locator) {
-        if (driver == null) setUpDriver();
-        return (MobileElement) byXpath(locator);
-    }
+	/**
+	 * Поиск мобильных элементов по xpath
+	 * Драйвер будет установлен автоматически, если это не было сделано ранее
+	 */
+	public static List<WebElement> WebElementsByXpath(String locator) {
+		if (driver == null) setUpDriver();
+		return driver.findElements(By.xpath(locator));
+	}
 
-    /**
-     * Поиск мобильных элементов по xpath
-     * Драйвер будет установлен автоматически, если это не было сделано ранее
-     */
-    public static List<MobileElement> mobileElementsByXpath(String locator) {
-        if (driver == null) setUpDriver();
-        return driver.findElementsByXPath(locator);
-    }
+	/**
+	 * Скролл до первого элемента, содержащего переданный текст
+	 *
+	 * @param text - текст, который должен быть в искомом элементе
+	 * @return - элемент, содержащий text
+	 */
+	public static WebElement scrollToElementWithText(String text) {
+		return driver
+				.findElement(new AppiumBy.ByAndroidUIAutomator("new UiScrollable(" +
+						"new UiSelector().scrollable(true).instance(0)).scrollIntoView(" +
+						"new UiSelector().textContains(\"" + text + "\").instance(0))"));
+	}
 
-    /**
-     * Скролл до первого элемента, содержащего переданный текст
-     * @param text - текст, который должен быть в искомом элементе
-     * @return - элемент, содержащий text
-     */
-    public static WebElement scrollToElementWithText(String text) {
-        return ((AndroidDriver) driver)
-                .findElementByAndroidUIAutomator("new UiScrollable(" +
-                        "new UiSelector().scrollable(true).instance(0)).scrollIntoView(" +
-                        "new UiSelector().textContains(\"" + text +"\").instance(0))");
-    }
+	/**
+	 * 1 "swipe" вниз экрана
+	 */
+	public static WebElement scrollDownOneTime() {
+		return driver
+				.findElement(new AppiumBy.ByAndroidUIAutomator("new UiScrollable(" +
+						"new UiSelector().scrollable(true).instance(0))" +
+						".scrollToEnd(1)"));
+	}
 
-    /**
-     * 1 "swipe" вниз экрана
-     */
-    public static WebElement scrollDownOneTime(){
-        return ((AndroidDriver) driver)
-                .findElementByAndroidUIAutomator("new UiScrollable(" +
-                        "new UiSelector().scrollable(true).instance(0))" +
-                        ".scrollToEnd(1)");
-    }
+	private static DesiredCapabilities getCapabilities() {
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		// iPhone Simulator, iPad Simulator, iPhone Retina 4-inch, Android Emulator, Galaxy S4 и т.д.
+		// На iOS, это должно быть одно  из допустимых устройств. На Android эта возможность в настоящее время игнорируется,
+		// но параметр является обязательным
+		capabilities.setCapability("deviceName", device.getDeviceName());
+		// Имя ОС на мобильном устройстве
+		capabilities.setCapability("platformName", device.getPlatformName());
+		// Версия ОС
+		capabilities.setCapability("platformVersion", device.getPlatformVersion());
+		// Уникальный идентификатор подключенного устройства
+		capabilities.setCapability("udid", device.getUdid());
+		// Java-пакет Android приложения, которое мы хотим запустить. (APK info)
+		capabilities.setCapability("appPackage", "net.metaquotes.economiccalendar");
+		// Имя activity которые мы хотим запустить из пакета, указанного выше. (APK info)
+		capabilities.setCapability("appActivity", "net.metaquotes.ui.MainActivity");
 
-    private static DesiredCapabilities getCapabilities() {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        // iPhone Simulator, iPad Simulator, iPhone Retina 4-inch, Android Emulator, Galaxy S4 и т.д.
-        // На iOS, это должно быть одно  из допустимых устройств. На Android эта возможность в настоящее время игнорируется,
-        // но параметр является обязательным
-        capabilities.setCapability("deviceName", device.getDeviceName());
-        // Имя ОС на мобильном устройстве
-        capabilities.setCapability("platformName", device.getPlatformName());
-        // Версия ОС
-        capabilities.setCapability("platformVersion", device.getPlatformVersion());
-        // Уникальный индефикатор подключенного устройства
-        capabilities.setCapability("udid", device.getUdid());
-        // Java-пакет Android приложения, которое мы хотим запустить. (APK info)
-        capabilities.setCapability("appPackage", "net.metaquotes.economiccalendar");
-        // Имя activity которые мы хотим запустить из пакета, указанного выше. (APK info)
-        capabilities.setCapability("appActivity", "net.metaquotes.ui.MainActivity");
+		return capabilities;
+	}
 
-        return capabilities;
-    }
+	public static void setUpDriver() {
+		quit();
 
-    public static void setUpDriver() {
-        quit();
+		String deviceName = System.getenv("deviceName");
 
-        String deviceName = System.getenv("deviceName");
+		if (deviceName == null)
+			device = Device.Pixel4;
+		else
+			device = Device.valueOf(deviceName);
 
-        if (deviceName == null)
-            device = Device.Pixel4;
-        else
-            device = Device.valueOf(deviceName);
+		step("Открыть приложение Tradays", () -> {
+			driver = new AndroidDriver(new URL(device.getUrl()), getCapabilities());
+		});
+	}
 
-        step("Открыть приложение Tradays", () -> {
-            driver = new AndroidDriver(new URL(device.getUrl()), getCapabilities());
-        });
-    }
-
-    /**
-     * Завершить работу драйвера
-     */
-    public static void quit() {
-        if (driver != null)
-            driver.quit();
-    }
+	/**
+	 * Завершить работу драйвера
+	 */
+	public static void quit() {
+		if (driver != null)
+			driver.quit();
+	}
 }
